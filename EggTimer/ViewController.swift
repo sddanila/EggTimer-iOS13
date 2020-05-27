6980fe20ecb1cb744e758ebe7dfa4c29192ef6c6
Danila Barton-Szabo <danilabarton-szabo@Enemy18s-MacBook-Pro.local>
2020-05-27 15:46:10 -0600

    add IBAction to egg hardness buttons

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes : [String : Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    let softTime : Int = 5
+    let mediumTime : Int = 7
+    let hardTime : Int = 12
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        
+        switch hardness {
+            case "Soft":
+                print(softTime)
+            case "Medium":
+                print(mediumTime)
+            case "Hard":
+                print(hardTime)
+            default:
+                print("Error")
+        }
+            
+            
+    }
+    
 }
