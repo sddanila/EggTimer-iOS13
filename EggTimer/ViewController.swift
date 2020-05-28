bc9f8036474066443e69e43a8aedbc6234daf778
Danila Barton-Szabo <danilabarton-szabo@Enemy18s-MacBook-Pro.local>
2020-05-28 14:55:08 -0600

    added timer to each button

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,28 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var eggLabel: UILabel!
     
     let eggTimes : [String : Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
     
-    let softTime : Int = 5
-    let mediumTime : Int = 7
-    let hardTime : Int = 12
+//    let softTime : Int = 300
+//    let mediumTime : Int = 420
+//    let hardTime : Int = 720
+    
+    let softTime : Int = 3
+    let mediumTime : Int = 4
+    let hardTime : Int = 7
+    
+    var secondsRemaining : Int? = 0
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
+        self.eggLabel.text = "How do you like your eggs?"
         
-        switch hardness {
-            case "Soft":
-                print(softTime)
-            case "Medium":
-                print(mediumTime)
-            case "Hard":
-                print(hardTime)
-            default:
-                print("Error")
-        }
-            
-            
+        timer.invalidate()
+        
+        secondsRemaining = eggTimes[hardness]!
+        
+        startTimer(secondsRemaining: self.secondsRemaining ?? 0)
     }
     
+    func startTimer(secondsRemaining : Int) {
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsRemaining! > 0 {
+                print ("\(String(describing: self.secondsRemaining)) seconds")
+                self.secondsRemaining! -= 1
+            } else {
+                timer.invalidate()
+                self.eggLabel.text = "Done!"
+            }
+        }
+    }
 }
